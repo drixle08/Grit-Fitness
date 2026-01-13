6ad04cb9245da2a82416c01a8c3dbfaf75855949
drixle08 <drixle08@gmail.com>
2026-01-13 07:48:04 +0300

    Initial commit

---
+++ server.py
@@ -0,0 +1,40 @@
+from http.server import SimpleHTTPRequestHandler, ThreadingHTTPServer
+from pathlib import Path
+import mimetypes
+import os
+
+ROOT = Path(__file__).resolve().parent
+
+
+class StaticHandler(SimpleHTTPRequestHandler):
+    extensions_map = {
+        **mimetypes.types_map,
+        ".js": "application/javascript",
+        ".mjs": "application/javascript",
+        ".css": "text/css",
+        ".html": "text/html",
+        ".json": "application/json",
+        ".webmanifest": "application/manifest+json",
+        ".svg": "image/svg+xml",
+        ".png": "image/png",
+        ".jpg": "image/jpeg",
+        ".jpeg": "image/jpeg"
+    }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=str(ROOT), **kwargs)
+
+
+def main():
+    host = os.environ.get("HOST", "127.0.0.1")
+    port = int(os.environ.get("PORT", "5173"))
+    server = ThreadingHTTPServer((host, port), StaticHandler)
+    print(f"Serving {ROOT} at http://{host}:{port}")
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == "__main__":
+    main()
